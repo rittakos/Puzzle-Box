b6028a6dbd4163efbb404794d8b1924e78945b6b
rittakos <akos.rittgasszer@gmail.com>
2022-07-01 10:11:10 +0200

    'init'

---
+++ PuzzleBox/PuzzleBox.ino
@@ -0,0 +1,18 @@
+/*
+ Name:		PuzzleBox.ino
+ Created:	2022-05-17 18:09:29
+ Author:	akosr
+*/
+
+#include <Servo.h>
+
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+
+}
+
+// the loop function runs over and over again until power down or reset
+void loop() {
+  
+}
